cc62fa679501309808dc2b0d034c23cedc63ea83
Manda728 <82278044+Manda728@users.noreply.github.com>
2021-06-08 22:11:17 +0200

    Added a search engine

--- src/app.js
+++ src/app.js
@@ -38,8 +38,20 @@ function displayTemperature(response) {
     );
     }
 
+function search(city) {
 let apiKey = "3cd1f0f8e060884deb552ea3639643b9";
-let city = "New York"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+ axios.get(apiUrl).then(displayTemperature);  
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
