1a7c1538b9e232d7466039797ce2b0823f620449
Manda728 <82278044+Manda728@users.noreply.github.com>
2021-06-15 23:19:43 +0200

    Added new function forecast

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ return `${day} ${hours}:${minutes}`;
 }
 
 function displayForecast() {
+   
 let forecastElement = document.querySelector("#forecast")
 
 let forecastHTML = `<div class="row">`;
@@ -42,6 +43,15 @@ days.forEach(function(day) {
      forecastHTML = forecastHTML + `</div>`;
      forecastElement.innerHTML = forecastHTML;
 }
+
+function getForecast(coordinates) {
+let apiKey = "3cd1f0f8e060884deb552ea3639643b9";    
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+
+axios.get(apiUrl).then(displayForecast);  
+}
+
 
 function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
@@ -70,6 +80,12 @@ function displayTemperature(response) {
     iconElement.setAttribute(
         "alt", response.data.weather[0].description
     );
+
+    getForecast(response.data.coord);
+   
+
+
+
     }
 
 function search(city) {
